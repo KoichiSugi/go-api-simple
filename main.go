7d36d0643b24888b3b9073eee58ae4454e9cd04b
Koichi Sugi <koichi.sugi01@gmail.com>
2021-10-13 21:39:28 +1100

    update

--- main.go
+++ main.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"emp-simple/config"
-	"emp-simple/service"
-	"fmt"
-	"log"
-	"net/http"
+	"emp-simple/routes"
 )
 
-func logging(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.URL.Path)
-		f(w, r)
-	}
-}
+// func logging(f http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		log.Println(r.URL.Path)
+// 		f(w, r)
+// 	}
+// }
 
 func main() {
 	config.ConnectDb()
 	defer config.Db.Close()
-
-	http.HandleFunc("/", service.GetAllEmployees)
-	http.HandleFunc("/test", logging(service.GetAllEmployees))
-	fmt.Println("listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	routes.Router()
+	// http.HandleFunc("/", service.GetAllEmployees)
+	// http.HandleFunc("/test", logging(service.GetAllEmployees))
+	// fmt.Println("listening on port 8080")
+	// http.ListenAndServe(":8080", nil)
 
 }
--- service/service.go
+++ service/service.go
@@ -1,7 +1,58 @@
 package service
 
-import "net/http"
+import (
+	"emp-simple/config"
+	"emp-simple/data"
+	"log"
+	"net/http"
 
-type ServiceImpl interface {
-	GetAllEmployees(w http.ResponseWriter, r *http.Request)
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllEmployees(c *gin.Context) {
+	rows, err := config.Db.Query("SELECT * FROM employee")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var emp data.Employee
+
+		if err := rows.Scan(&emp.Id, &emp.FirstName, &emp.MiddleName,
+			&emp.LastName, &emp.Gender, &emp.Salary, &emp.DOB, &emp.Email,
+			&emp.Phone, &emp.State, &emp.Postcode, &emp.AddressLine1, &emp.AddressLine2,
+			&emp.TFN, &emp.SuperBalance); err != nil {
+			log.Fatal(err)
+			c.IndentedJSON(http.StatusInternalServerError, nil)
+		}
+		c.IndentedJSON(http.StatusOK, emp)
+
+	}
+	if c.Err(); err != nil {
+		log.Fatal(c)
+	}
+
 }
+
+// func GetAllEmployees(w http.ResponseWriter, r *http.Request) {
+// 	rows, err := config.Db.Query("SELECT * FROM employee")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var emp data.Employee
+
+// 		if err := rows.Scan(&emp.Id, &emp.FirstName, &emp.MiddleName,
+// 			&emp.LastName, &emp.Gender, &emp.Salary, &emp.DOB, &emp.Email,
+// 			&emp.Phone, &emp.State, &emp.Postcode, &emp.AddressLine1, &emp.AddressLine2,
+// 			&emp.TFN, &emp.SuperBalance); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		json.NewEncoder(w).Encode(emp)
+// 	}
+// 	if rows.Err(); err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// }
